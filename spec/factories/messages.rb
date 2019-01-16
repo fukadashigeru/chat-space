a3eafaf1165476b031d793652b47f12799b8824c
fukadashigeru <sakusaku721@gmail.com>
2019-01-16 14:43:04 +0900

    Test

---
+++ spec/factories/messages.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :message do
+    content Faker::Lorem.sentence
+    image File.open("#{Rails.root}/public/uploads/message/image/8/スクリーンショット_2019-01-15_19.52.04.png")
+    user
+    group
+  end
+end
