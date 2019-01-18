3518bcb5388e658768e8851a1cfb5ff68e9c24cb
fukadashigeru <sakusaku721@gmail.com>
2019-01-18 16:53:41 +0900

    Asynchronous_function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(function(){
+  function buildHTML(message){
+    var date = new Date()
+    // console.log(message)
+    var url = message.image.url
+    // console.log(url)
+    var image = url == null ? '' : '<img class="lower-message__image" src= '+ url + '/>';
+    console.log(image)
+    var html = `<div class='message'>
+                  <div class='upper-message'>
+                    <div class='upper-message__user-name'>
+                      ${message.user_name}
+                    </div>
+                  <div class='upper-message__date'>
+                    ${ date.getFullYear() }/${ ("0"+(date.getMonth() + 1)).slice(-2) }/${ ("0"+date.getDate()).slice(-2) }  ${ ("0"+date.getHours()).slice(-2) }:${ ("0"+date.getMinutes()).slice(-2) }
+                  </div>
+                </div>
+                <div class='lower-meesage'>
+                  <p class='lower-message__content'>
+                    ${message.content}
+                  </p>
+                  ${image}
+                </div>`
+    return html;
+  }
+  $('#new_message').on('wwwwturbolinks: submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href + ''
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      var h = $('.messages').get(0).scrollHeight;
+      $('.messages').append(html)
+      $('.form__message').val('')
+      $('#message_image').val('')
+      $('.form__submit').prop('disabled', false);
+      $('.messages').animate({
+        scrollTop: h
+      })
+    })
+    .fail(function(){
+      alert('メッセージを入力してください。');
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+});
