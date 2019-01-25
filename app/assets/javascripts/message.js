bc4c7ea872ac75f8946233d7f9e52ddbcb8461e7
fukadashigeru <sakusaku721@gmail.com>
2019-01-25 18:27:59 +0900

    modify_message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,71 +1,69 @@
 $(function(){
-  $(function(){
-  setInterval(update, 10000);
-  });
-  function update(){
-    var last_message_id = $('.message:last').attr('value');
-    $.ajax({
-      url: location.href,
-      type: 'GET',
-      data: {
-        last_message_id: last_message_id
-      },
-      dataType: 'json'
-    })
-    .done(function(new_messages) {
-      new_messages.forEach(function(message){
-        var html = buildHTML(message);
-        $('.messages').append(html)
-      });
-    })
-  }
-  function buildHTML(message){
-    var date = new Date()
-    var url = message.image.url
-    var created_at = message.created_at
-    var image = url == null ? '' : '<img class="lower-message__image" src= '+ url + '/>';
-    var html = `<div class='message' value=${message.id}>
-                  <div class='upper-message'>
-                    <div class='upper-message__user-name'>
-                      ${message.user_name}
-                    </div>
-                  <div class='upper-message__date'>
-                    ${ created_at }
-                  </div>
-                </div>
-                <div class='lower-meesage'>
-                  <p class='lower-message__content'>
-                    ${message.content}
-                  </p>
-                  ${image}
-                </div>`
-    return html;
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var href = window.location.href + ''
-    $.ajax({
-      url: href,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
+setInterval(update, 10000);
+});
+function update(){
+  var last_message_id = $('.message:last').attr('value');
+  $.ajax({
+    url: location.href,
+    type: 'GET',
+    data: {
+      last_message_id: last_message_id
+    },
+    dataType: 'json'
+  })
+  .done(function(new_messages) {
+    new_messages.forEach(function(message){
+      var html = buildHTML(message);
       $('.messages').append(html)
-      $('.form__message').val('')
-      $('#message_image').val('')
-      $('.form__submit').prop('disabled', false);
-      $('.messages').animate({
-        scrollTop: $('.messages').get(0).scrollHeight
-      })
-    })
-    .fail(function(){
-      alert('メッセージを入力してください。');
-      $('.form__submit').prop('disabled', false);
+    });
+  })
+}
+function buildHTML(message){
+  var date = new Date()
+  var url = message.image.url
+  var created_at = message.created_at
+  var image = url == null ? '' : '<img class="lower-message__image" src= '+ url + '/>';
+  var html = `<div class='message' value=${message.id}>
+                <div class='upper-message'>
+                  <div class='upper-message__user-name'>
+                    ${message.user_name}
+                  </div>
+                <div class='upper-message__date'>
+                  ${ created_at }
+                </div>
+              </div>
+              <div class='lower-meesage'>
+                <p class='lower-message__content'>
+                  ${message.content}
+                </p>
+                ${image}
+              </div>`
+  return html;
+}
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var href = window.location.href + ''
+  $.ajax({
+    url: href,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html)
+    $('.form__message').val('')
+    $('#message_image').val('')
+    $('.form__submit').prop('disabled', false);
+    $('.messages').animate({
+      scrollTop: $('.messages').get(0).scrollHeight
     })
   })
-});
+  .fail(function(){
+    alert('メッセージを入力してください。');
+    $('.form__submit').prop('disabled', false);
+  })
+})
